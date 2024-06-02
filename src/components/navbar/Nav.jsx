651a43002be0ad9ea5c912fe7bf52b1bbf5b694d
madhushrabadas <madhushraba21@gmail.com>
2024-06-03 02:16:16 +0530

    prisma added and loggedin user saved in mongoatlas

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
@@ -31,10 +32,8 @@ const navData = [
 ];
 
 const Nav = () => {
-  const logout = () => {
-    console.log("logout");
-    console.log("logout");
-  };
+  const { status } = useSession();
+  console.log(status);
   return (
     <div className="bg-rose-200 text-rose-800 flex justify-between items-center p-4 px-20">
       <Link href={"/"} className="left">
@@ -46,9 +45,16 @@ const Nav = () => {
             <a href={user.path}>{user.name}</a>
           </li>
         ))}
-        <div onClick={logout} className="p">
-          logout
-        </div>
+        {status === "notauthenticated" ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <>
+            <Link href="/write">Write</Link>
+            <span onClick={signOut} className="flex p-2 bg-rose-50">
+              Logout
+            </span>
+          </>
+        )}
       </div>
     </div>
   );
